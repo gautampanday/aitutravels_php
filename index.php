388fcc446471c5db767075f56959b03e7a8894b1
gautampanday <gautamdcsk@gmail.com>
2016-06-01 01:20:37 +0530

    seed

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  
+  <?php include_once('header.php'); ?>
+  <body>
+    <?php include('login.php'); ?>
+    <script src="js/lib/jquery-2.1.4.min.js"></script>
+    <script src="js/lib/jquery-ui.js"></script>
+    <script src='assets/js/jquery.nicescroll.min.js'></script>
+    <script src='assets/js/wow.min.js'></script>
+    <script src='assets/js/jquery.loadmask.min.js'></script>
+    <script src='assets/js/jquery.accordion.js'></script>
+    <script src='assets/js/materialize.js'></script>
+    <script src="js/lib/bootstrap.min.js"></script>
+    <script src="js/lib/materialize.js"></script>
+  </body>
+</html>
